1bc313f743511df47700ec7ed1807cec526b54cd
PinkZhi <zywoffice@163.com>
2020-06-29 08:56:14 +0800

    add resource file

--- WordCloud/test.py
+++ WordCloud/test.py
@@ -4,9 +4,9 @@ from WordCloudAssistant import *
 def main():
     wcAssistant = WordCloudAssistant()
 
-    with open('C:\\WorkSpace\\WordCloud\\minister.txt', 'r', encoding='utf8') as fp:
+    with open('C:\\WorkSpace\\Spider\\Resource\\minister.txt', 'r', encoding='utf8') as fp:
         text = fp.read()
-        wcAssistant.Generate(text = text, backgroundFile = 'C:\\WorkSpace\\WordCloud\\test1.jpg', isChines = True)
+        wcAssistant.Generate(text = text, backgroundFile = 'C:\\WorkSpace\\Spider\\Resource\\test1.jpg', isChines = True)
 
 if __name__ == '__main__':
     main()

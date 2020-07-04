5cd8fe09e1ab4e86b445b5c2dc5c39576a8c8228
PinkZhi <zywoffice@163.com>
2020-07-04 14:53:13 +0800

    parse locate

--- RentSpider/LianjiaSpider.py
+++ RentSpider/LianjiaSpider.py
@@ -1,31 +1,81 @@
 from bs4 import BeautifulSoup
 from SpiderBase import *
 import re
+import time
+import csv
+
+
+class RentInfo(object):
+    def __init__(self):
+        self.downtown = ''
+        self.subDowntown = ''
+        self.community = ''
+        self.area = 0
+        self.rent = 0
+    
+
+    def __repr__(self):
+        return '%s, %s, %s, %d, %d' % (self.downtown, self.subDowntown, self.community, self.rent, self.area)
+
 
 class LianjiaSpider(SpiderBase):
-    def HandleHtml(self, html):
-        """
-        计算每平米均价
-        """
-        soup = BeautifulSoup(html, features='html.parser')
-        houseList = soup.find(class_ = 'content__list').find_all(class_ = 'content__list--item--main')
+    def __init__(self):
+        SpiderBase.__init__(self)
+        self.rentMap = []
 
-        head = ['locate', 'rent', 'area']
-        with open('data.csv', 'a') as csvfile:
+
+    def SaveData(self):
+        if len(self.rentMap) == 0:
+            return
+
+        head = ['downtown', 'sub_downtown', 'community', 'rent', 'area']
+        with open('data.csv', 'w', encoding='utf8') as csvfile:
             f = csv.DictWriter(csvfile, fieldnames = head)
             f.writeheader()
+            for rentInfo in self.rentMap:
+                line = {
+                    'downtown': rentInfo.downtown, 
+                    'sub_downtown': rentInfo.subDowntown, 
+                    'community': rentInfo.community, 
+                    'rent': str(rentInfo.rent), 
+                    'area': str(rentInfo.area)
+                }
+                f.writerow(line)
 
-            for houseInfo in houseList:
-                areaStr = re.findall('[\d{1,}㎡]\d{1,}', houseInfo.find(class_='content__list--item--des').text)
-                rentStr = houseInfo.find(class_ = 'content__list--item-price').find('em').text
-                if areaStr and rentStr:
-                    line = {'rent': rentStr, 'area': areaStr[0], 'locate': 'bingjiang'}
-                    f.writerow(line)
+
+    def ParseInfo(self, houseInfo):
+        res = houseInfo.find(class_='content__list--item--des').find_all('a')
+        if res and len(res) == 3:
+            rentInfo = RentInfo()
+            rentInfo.downtown = res[0].text
+            rentInfo.subDowntown = res[1].text
+            rentInfo.community = res[2].text
+            rentStr = houseInfo.find(class_ = 'content__list--item-price').find('em').text
+            rentInfo.rent = int(rentStr)
+            detail = houseInfo.find(class_='content__list--item--des').text 
+            text = re.findall('(\d{1,}㎡)', detail)
+            areaStr = text[0][0:-1]
+            rentInfo.area = int(areaStr)
+            self.rentMap.append(rentInfo)
+
+
+    def HandleHtml(self, html):
+        soup = BeautifulSoup(html, features='html.parser')
+        houseList = soup.find(class_ = 'content__list').find_all(class_ = 'content__list--item--main')
+        for houseInfo in houseList:
+            self.ParseInfo(houseInfo)
 
 
     def Crawl(self):
-        location = ['bingjiang', 'xiaoshan']
-        for item in location:
-            url = 'https://hz.lianjia.com/zufang/%s/pg2/#contentList' % item
+        for index in range(1,100):
+            url = 'https://hz.lianjia.com/zufang/pg%drco11/#contentList' % index
+            if index == 1:
+                url = 'https://hz.lianjia.com/zufang/rco11/#contentList'
             html = self.GetHtml(url)
-            self.HandleHtml(html)
+            if html:
+                self.HandleHtml(html)
+            else:
+                print(self.GetLastError())
+            time.sleep(3)
+        self.SaveData()
+        
--- RentSpider/SpiderBase.py
+++ RentSpider/SpiderBase.py
@@ -1,39 +1,56 @@
 import requests
-import csv
+import sys
+import re
+import bs4
 
 class SpiderBase(object):
     def __init__(self):
+        self.__last_error = ''
         self.__headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
-        self.__proxies= {}
-
-
-    def Create(self, headers: dict, proxies: dict = None):
-        self.__headers = headers
-        if proxies:
-            self.__proxies = proxies
-
-
-    def SetHeaders(self, headers):
-        self.__headers = headers
-
-
-    def SetProxies(self, proxies):
-        self.__proxies = proxies
-
-
-    def GetHtml(self, url):
-        try:
-            if len(self.__proxies) > 0:
-                res = requests.get(url, headers = self.__headers, proxies = self.__proxies)
-            else:
-                res = requests.get(url, headers = self.__headers)
-            
-            if res.status_code == 200:
-                return res.text
-            else:
-                return 'get html from %s return %d' % (url, res.status_code)
-
-        except requests.exceptions:
-            return 'get html from %s request error occur' % url
+        self.__proxies = {}
 
     
+    def SetHeaders(self, headers: dict):
+        if headers and len(headers) > 0:
+            self.__headers = headers
+    
+
+    def SetProxies(self, proxies: dict):
+        if proxies and len(proxies) > 0:
+            self.__proxies = proxies
+
+    
+    def GetLastError(self)->str:
+        return self.__last_error
+
+    
+    def HandleResponse(self, response):
+        if response is None:
+            return
+        
+        if response.status_code == 200:
+            response.encoding = response.apparent_encoding
+            self.__last_error = ''
+            return response.text
+        else:
+            self.__last_error = 'crawl response %d' % response.status_code
+            return ''
+
+
+    def GetHtml(self, url: str)->str:
+        try:
+            html = ''
+            if len(self.__proxies) > 0:
+                response = requests.get(url, headers = self.__headers, proxies = self.__proxies)
+                html = self.HandleResponse(response)
+            else:
+                response = requests.get(url, headers = self.__headers)
+                html = self.HandleResponse(response)
+            return html
+
+        except requests.exceptions():
+            self.__last_error = 'crawl request fail'
+            return ''
+        except:
+            self.__last_error = 'crawl system fail: %s' % sys.exc_info()[0]
+            return ''

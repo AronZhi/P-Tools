55c629c0e90f2f3a17ec99dcc23431fed9bfc02d
PinkZhi <zywoffice@163.com>
2020-07-06 13:02:08 +0800

    delete practice file

--- Practice/Decoration.py
+++
@@ -1,6 +0,0 @@
-class Decoration(object):
-    def GetHeaders(self):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
-        }
-        return headers

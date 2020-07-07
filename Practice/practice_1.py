6db8b06e3c4d9d111fe684bf6af2b905f00f603b
PinkZhi <zywoffice@163.com>
2020-07-07 18:26:29 +0800

    add multi process practice

--- Practice/practice_1.py
+++ Practice/practice_1.py
@@ -1,8 +1,11 @@
 import requests
 
 def test_1():
+    """
+    通过requests库请求网页数据
+    """
     try:
-        response = requests.get('https://book.douban.com/')
+        response = requests.get('http://top.baidu.com/buzz?b=341&c=513&fr=topcategory_c513')
         if response.status_code == 200:
             print(response.text)
         else:
@@ -15,10 +18,13 @@ def test_1():
 
 
 def test_2():
+    """
+    增加request header来规避反爬
+    """
     try:
         header = {'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
-        response = requests.get('https://book.douban.com/', headers = header)
+        response = requests.get('http://top.baidu.com/buzz?b=341&c=513&fr=topcategory_c513', headers = header)
         if response.status_code == 200:
             print(response.text)
         else:
@@ -30,10 +36,30 @@ def test_2():
         print("except:",e)
     return
 
+def test_3():
+    """
+    修改编码来显示中文
+    """
+    try:
+        header = {'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+        response = requests.get('http://top.baidu.com/buzz?b=341&c=513&fr=topcategory_c513', headers = header)
+        if response.status_code == 200:
+            response.encoding = response.apparent_encoding
+            print(response.text)
+        else:
+            print('response:%d' % response.status_code)
+
+    except requests.exceptions as requestErr:
+        print('request err:', requestErr)
+    except Exception as e:
+        print("except:",e)
+    return
 
 def main():
     #test_1()
-    test_2()
+    #test_2()
+    test_3()
 
 
 if __name__ == '__main__':
--- Practice/practice_2.py
+++ Practice/practice_2.py
@@ -1,11 +1,39 @@
 import requests
 import re
+import bs4
+
+def GetHtml():
+    try:
+        header = {'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+        response = requests.get('http://top.baidu.com/buzz?b=341&c=513&fr=topcategory_c513', headers = header)
+        if response.status_code == 200:
+            response.encoding = response.apparent_encoding
+            return response.text
+        else:
+            return 'response:%d' % response.status_code
+
+    except requests.exceptions as requestErr:
+        return 'request err:' + requestErr
+    except Exception as e:
+        return 'except:' + e
 
 def test_1():
+    html = GetHtml()
+    lst = re.findall('[\u4e00-\u9fa5]{4,}', html)
+    print(lst)
     return
 
+def test_2():
+    html = GetHtml()
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    lst = soup.find(class_ = 'list-table').find_all(class_ = 'list-title')
+    for item in lst:
+        print(item.text)
+
 def main():
-    test_1()
+    #test_1()
+    test_2()
     return
 
 if __name__ == '__main__':

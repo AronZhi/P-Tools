68ead8019e6e3d823e56ff04453571b1b26b3b84
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2021-05-24 17:39:20 +0800

    update code

--- Common/LogMember.py
+++ Common/LogMember.py
@@ -1,3 +1,3 @@
-from LogComponent.LogHandler import LogHandler
+from LogHandler import *
 
 g_main_log = LogHandler('main')
--- Common/Mark.py
+++ Common/Mark.py
@@ -1,4 +1,5 @@
 import logging
+import json
 from functools import wraps
 
 PrintTool = None
@@ -22,4 +23,27 @@ def LogInterface(func):
         Printf(func.__name__ + ' end')
         return ret
     return with_logging
-        
+
+def FormatFuncAsJson(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        data = {"func": func.__name__, "kwargs": kwargs}
+        jsonData = json.dumps(data)
+        print(jsonData)
+        ret = func(*args, **kwargs)
+        return ret
+    return with_logging
+
+"""
+@FormatFuncAsJson
+def test(test):
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+    print(test)
+    #print(kwargs)
+    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+if __name__ == "__main__":
+    from LogMember import g_main_log
+    SetPrintTool(g_main_log)
+    test(test=1)
+"""

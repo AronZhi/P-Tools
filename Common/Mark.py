6fa5ebe71a83fad9915b2030e762b4f740685219
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2021-06-04 10:06:59 +0800

    update code

--- Common/Mark.py
+++ Common/Mark.py
@@ -1,41 +1,39 @@
-import logging
 import json
 from functools import wraps
 
-PrintTool = None
-
-def SetPrintTool(writer:logging.Logger = None):
-    global PrintTool
-    PrintTool = writer
-
-def Printf(text):
-    global PrintTool
-    if PrintTool:
-        PrintTool.info(text)
-    else:
-        print(text)
-
-def LogInterface(func):
-    @wraps(func)
-    def with_logging(*args, **kwargs):
-        Printf(func.__name__ + ' start')
-        ret = func(*args, **kwargs)
-        Printf(func.__name__ + ' end')
-        return ret
-    return with_logging
-
-def FormatFuncAsJson(func):
-    @wraps(func)
-    def with_logging(*args, **kwargs):
-        data = {"func": func.__name__, "kwargs": kwargs}
-        jsonData = json.dumps(data)
+class Mark(object):
+    @classmethod
+    def handleFuncInfo(cls, jsonData):
         print(jsonData)
-        ret = func(*args, **kwargs)
-        return ret
-    return with_logging
+
+    @classmethod
+    def MarkFuncInfo(cls, func):
+        @wraps(func)
+        def fetchInfo(*args, **kwargs):
+            data = {"func": func.__name__, "kwargs": kwargs}
+            jsonData = json.dumps(data)
+            cls.handleFuncInfo(jsonData)
+            ret = func(*args, **kwargs)
+            return ret
+        return fetchInfo
+    
+    @classmethod
+    def handleFuncProcess(cls, data):
+        print(data)
+    
+    @classmethod
+    def MarkFuncProcess(cls, func):
+        @wraps(func)
+        def recordProcess(*args, **kwargs):
+            cls.handleFuncProcess(func.__name__ + ' start')
+            ret = func(*args, **kwargs)
+            cls.handleFuncProcess(func.__name__ + ' end')
+            return ret
+        return recordProcess
 
 """
-@FormatFuncAsJson
+@Mark.MarkFuncProcess
+@Mark.MarkFuncInfo
 def test(test):
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     print(test)
@@ -43,7 +41,5 @@ def test(test):
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 if __name__ == "__main__":
-    from LogMember import g_main_log
-    SetPrintTool(g_main_log)
     test(test=1)
 """

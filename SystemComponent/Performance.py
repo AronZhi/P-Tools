7ff43bd30c75a4e8a5a522b9c5507c52c64a114b
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2021-07-07 11:22:19 +0800

    update code

--- SystemComponent/Performance.py
+++ SystemComponent/Performance.py
@@ -1,8 +1,9 @@
+from time import sleep, time
 import psutil
 
 class PerformanceInfo(object):
     def GetCpuPercent(self):
-        return psutil.cpu_percent(interval=0.5, percpu=True)
+        return psutil.cpu_percent(interval=2, percpu=True)
     
     def GetMemoryPercent(self):
         return psutil.virtual_memory().percent
@@ -10,17 +11,25 @@ class PerformanceInfo(object):
     def GetProcessRunInfo(self, process_name):
         cpu_percent = 0
         memory_usage = 0
+        process_list =[]
         for process in psutil.process_iter():
             try:
-                if process_name.lower() in process.name().lower():
-                    cpu_percent += process.cpu_percent()
+                if process_name.lower() == process.name().lower():
+                    process_list.append(process)
                     memory_usage += process.memory_info().rss
             except (psutil.AccessDenied, psutil.ZombieProcess):
                 continue
+        for process in process_list:
+            cpu_percent += process.cpu_percent(None)
+        sleep(2)
+        for process in process_list:
+            cpu_percent += process.cpu_percent(None)
         return cpu_percent, memory_usage
 
+"""
 if __name__ == "__main__":
     performanceInfo = PerformanceInfo()
     print(performanceInfo.GetCpuPercent())
     print(performanceInfo.GetMemoryPercent())
     print(performanceInfo.GetProcessRunInfo('firefox'))
+"""

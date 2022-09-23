6e35fa475f7ae70ce9e351f258431aaea4d9bd42
Yunwei Zhi -X (yunzhi - Hileap at Cisco) <yunzhi@cisco.com>
2022-09-23 10:02:59 +0800

    update code

--- Common/Mark.py
+++ Common/Mark.py
@@ -1,56 +1,30 @@
 import json
 from functools import wraps
 
-class Mark(object):
-    @classmethod
-    def handleFuncInfo(cls, jsonData):
-        print(jsonData)
-
-    @classmethod
-    def MarkFuncInfo(cls, func):
-        @wraps(func)
-        def fetchInfo(*args, **kwargs):
-            data = {"module": func.__module__, "func": func.__name__, "kwargs": kwargs, "args": args}
-            jsonData = json.dumps(data)
-            cls.handleFuncInfo(jsonData)
+def traceFunction(func):
+    @wraps(func)
+    def traceFunctionWrapper(*args, **kwargs):
+        trace = dict()
+        trace['module'] = func.__module__
+        trace['func'] = func.__name__
+        trace['args'] = args
+        trace['kwargs'] = kwargs
+        try:
             ret = func(*args, **kwargs)
+            #trace['result'] = ret
             return ret
-        return fetchInfo
-    
-    @classmethod
-    def handleFuncProcess(cls, data):
-        print(data)
-    
-    @classmethod
-    def MarkFuncProcess(cls, func):
-        @wraps(func)
-        def recordProcess(*args, **kwargs):
-            cls.handleFuncProcess(func.__name__ + ' start')
-            ret = func(*args, **kwargs)
-            cls.handleFuncProcess(func.__name__ + ' end')
-            return ret
-        return recordProcess
+        except Exception as e:
+            trace['error'] = str(e)
+        finally:
+            print(json.dumps(trace))
+    return traceFunctionWrapper
 
 """
-@Mark.MarkFuncProcess
-@Mark.MarkFuncInfo
-def test1(a, b, c, *args):
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print(args)
-    #print(kwargs)
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+@traceFunction
+def div(a, b):
+    return a/b
 
-@Mark.MarkFuncProcess
-@Mark.MarkFuncInfo
-def test2(*args, a, b, c):
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    print(args)
-    #print(kwargs)
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-
-test1(1, 2, 3, 1, 2, 1)
-#test1(1, 2, 3, a=1, b=2, c=1)
-#test1(a=1, b=2, c=3, 1, 2, 1)
-test2(1, 2, 3, a=1, b=2, c=1)
-#test2(1, 2, 3, 1, 2, 1)
+if __name__ == "__main__":
+    div(1, 0)
+    div(1, 2)
 """

5ac339773dd8271c92afb60c7afa699166bd7134
Aron_Zhi <azhi@nextlabs.com>
2022-12-21 10:52:58 +0800

    use aiohttp web server instead of tornado

--- NetComponent/AsyncHttpServer/handler.py
+++ NetComponent/AsyncHttpServer/handler.py
@@ -1,21 +1,21 @@
-import json
-from tornado.web import RequestHandler
+import asyncio
+from aiohttp import web
+import weakref
 
+async def on_shutdown(app: web.Application) -> None:
+    print('on shutdown')
+    sockets = weakref.WeakSet()
+    for ws in sockets:
+        await ws.close()
 
-class Handler(RequestHandler):
-    def parseRequest(self):
-        data = dict()        
-        if self.request.headers.get('Content-Type') == 'application/json':
-            data = json.loads(self.request.body.decode())
-        else:
-            for key in self.request.arguments:
-                data[key] = self.get_argument(key)
-        return data
-    
-    def sendJson(self,
-                   comment,
-                   **kwargs):
-        kwargs['comment'] = comment
-        self.set_header()
-        self.write(json.dumps(kwargs))
-        
+async def hello(request: web.Request):
+    """
+    # request body json form, and return json response
+    data = await request.json()
+    data["hello"] = "Hello, this is admin server"
+    return web.json_response(text=json.dumps(data))
+    """
+    return web.Response(text="Hello, this is admin server")
+
+async def stop(request: web.Request):
+    exit(0)
--- NetComponent/AsyncHttpServer/server.py
+++ NetComponent/AsyncHttpServer/server.py
@@ -1,23 +1,23 @@
-import tornado.web
-import tornado.httpserver
-import tornado.ioloop
+import asyncio
+from aiohttp import web
+from handler import *
 
-class Server(object):
-    @classmethod
-    def start(cls,
-              port: int,
-              protocol: list):
-        """
-        windows环境python3.7及以上版本tornado使用asyncio抛出NotImplementedError异常，
-        试用前需要以下处理
-        if psutil.WINDOWS and sys.version_info >= (3,7):
-            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-        """
-        app = tornado.web.Application(protocol)
-        httpServer = tornado.httpserver.HTTPServer(app)
-        httpServer.listen(port)
-        tornado.ioloop.IOLoop.instance().start()
-    
-    @classmethod
-    def stop(cls):
-        tornado.ioloop.IOLoop.instance().stop()
+class AdminServer(object):
+    def __init__(self, port=8888, event_loop = None):
+        if event_loop is None:
+            """
+            windows环境python3.7及以上版本tornado使用asyncio可能抛出NotImplementedError异常, 使用前需要以下处理
+            if psutil.WINDOWS and sys.version_info >= (3,7):
+                asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+            """
+            self._event_loop = asyncio.get_event_loop()
+        else:
+            self._event_loop = event_loop
+        self._port = port
+        self._app = web.Application()
+        self._app.on_shutdown.append(on_shutdown)
+        self._app.router.add_get('/hello', hello)
+        self._app.router.add_get('/stop', stop)
+
+    def run(self):
+        web.run_app(app=self._app, port=self._port, loop=self._event_loop)
